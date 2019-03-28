fd8ba63e4cd329efaa56e3810abf64425625c28d
Karm Sidhu <karmveer.s.sidhu@outlook.com>
2019-03-27 21:43:10 -0600

    Refactoring and Labelling new test images

--- API_Project/src/main.py
+++ API_Project/src/main.py
@@ -34,14 +34,14 @@ def make_master_files(fm, aws_data, gcv_data, aws_dir:str, gcv_dir:str):
 
 def main():
 	# Set directories -------
-	aws_label_dir = "API_Project/AWS-LabelledData-Rescale150"
-	gcv_label_dir = "API_Project/GCV-LabelledData-Rescale150"
-	img_dir = "ImageData-Rescale150"
+	aws_label_dir = "API_Project/AWS-Standard-Labels"
+	gcv_label_dir = "API_Project/GCV-Standard-Labels"
+	img_dir = "Images/Standard-Size"
 	
 	# Instantiate Objects -------
 	fm = FileManager()
 	aws_Rekognition = Rekognition()
-	gc_Vision = Vision()
+	# gc_Vision = Vision()
 
 	# Load Previously Labelled Data -------
 	aws_data, gcv_data = load_prev_data(fm, aws_dir = aws_label_dir, 
@@ -50,18 +50,18 @@ def main():
 	# Convert all images to bytes -------
 	aws_img_data = fm.image_to_bytes(src_dir = img_dir, 
 		file_names = fm.get_file_names(dir = img_dir))
-	gcv_img_data = fm.image_to_bytes(src_dir = img_dir, 
-		file_names = fm.get_file_names(dir = img_dir))
+	# gcv_img_data = fm.image_to_bytes(src_dir = img_dir, 
+	# 	file_names = fm.get_file_names(dir = img_dir))
 
 	# Remove already labelled data -------
 	remove_repeats_rekog(aws_data, aws_img_data)
 	# remove_repeats_vision(gcv_data, gcv_img_data)
 
 	# Label using Rekognition -------
-	aws_response = aws_Rekognition.label_images(byte_imgs = aws_img_data, img_nums = 200)
-	if aws_response != None:
-		fm.to_json(json_dict = aws_response, 
-			dir_name = aws_label_dir)
+	# aws_response = aws_Rekognition.label_images(byte_imgs = aws_img_data, img_nums = 600)
+	# if aws_response != None:
+	# 	fm.to_json(json_dict = aws_response, 
+	# 		dir_name = aws_label_dir)
 	
 	# Label using Vision -------
 	# gcv_response = gc_Vision.label_images(byte_imgs = gcv_img_data, img_nums = 10)
@@ -70,7 +70,7 @@ def main():
 	# 		dir_name = gcv_label_dir)
 
 	# Create Master Files that consolidates all labelled data -------
-	# make_master_files(fm,aws_data,gcv_data, aws_label_dir, gcv_label_dir)
+	make_master_files(fm,aws_data,gcv_data, aws_label_dir, gcv_label_dir)
 
 
 main()
