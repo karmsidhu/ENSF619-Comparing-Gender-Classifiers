b644c72f80debfcaca976a92ca93d51f2d2eef3f
karmsidhu <karmveer.s.sidhu@outlook.com>
2019-05-14 19:13:31 -0600

    Added documentation

--- ImageLabeling/src/Labeller.py
+++ ImageLabeling/src/Labeller.py
@@ -1,14 +1,37 @@
+"""
+Script to aid in labelling images
+
+Was created to help a face by race but was unused due to a heavy uneven
+distribution in the dataset
+
+Dependencies:
+    - json
+    - os
+"""
+
 import json
 import os
 
-
 def get_file_names(dir:str = "ImageData"):
     """
-    Gets all file names in dir
+    Gets all file names in provided directory
+
+    Args:
+        dir: Directory containing the images
+    
+    Returns:
+        List of all files in provided directory
     """
     return os.listdir(dir)
 
 def to_json_file(label_dict:dict, dir:str = "ImageLabeling"):
+    """
+    Writes provided dictionary to provided directory in JSON format
+
+    Args:
+        label_dict: Dictionary to be written to file as LabeledImages.json
+        dir: Directory to where file will be written
+    """
     json_string = json.dumps(label_dict, sort_keys=True, indent=4, 
             separators=(',',':'))
 
@@ -16,11 +39,26 @@ def to_json_file(label_dict:dict, dir:str = "ImageLabeling"):
         f.write(json_string)
 
 def load_json_file(file_loc:str = "ImageLabeling/LabeledImages.json"):
+    """
+    Loads JSON file as a dictionary
+
+    Args:
+        file_loc: Location of JSON file
+
+    Returns:
+        Dictionary based on the provided file
+    """
     with open(file_loc, "r") as f:
         data = json.load(f)
     return data
 
 def main():
+    """
+    Begins labelling process
+
+    Will first load already labelled images and then start labelling
+    process for non-labelled images
+    """
     img_dict = load_json_file()
     all_imgs = get_file_names()
     imgs = []
@@ -47,4 +85,4 @@ def main():
             break
     to_json_file(img_dict)
 
-main()
+# main()
